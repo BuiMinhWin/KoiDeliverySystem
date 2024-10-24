1ffdaa03ed469a6330c7efb33ad3a129b58c425d
Minh Thang <bmthang2004@gmail.com>
2024-10-24 14:15:35 +0700

    add vercel.json

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
-const REST_API_BASE_URL = "http://koideliverysystem.id.vn:8080/api/accounts";
-const REST_API_BASE_URL2 = "http://koideliverysystem.id.vn:8080/api/Google/loginGG";
-const REST_API_PASSWORD_URL = "http://koideliverysystem.id.vn:8080/api/auth";
+
+const REST_API_BASE_URL = "/api/accounts";
+const REST_API_BASE_URL2 = "/api/Google/loginGG";
+const REST_API_PASSWORD_URL = "/api/auth";
 
 export const listAccount = () => {
   return axios.get(REST_API_BASE_URL);
@@ -12,21 +13,17 @@ export const createAccount = (account) => {
   return axios.post(REST_API_BASE_URL + '/register', account);
 };
 
-
 export const getAccount = (accountId) => {
   return axios.get(REST_API_BASE_URL + '/' + accountId);
 };
-
 
 export const updateAccount = (accountId, account) => {
   return axios.put(REST_API_BASE_URL + '/' + accountId, account);
 };
 
-
 export const deleteAccount = (accountId) => {
   return axios.delete(REST_API_BASE_URL + '/' + accountId);
 };
-
 
 export const loginAccount = (loginData) => {
   return axios.post(REST_API_BASE_URL + '/login', loginData, {
@@ -35,18 +32,14 @@ export const loginAccount = (loginData) => {
   });
 };
 
-
 export const googleLogin = (account) => {
-  return axios.post(REST_API_BASE_URL2,account);
-   
+  return axios.post(REST_API_BASE_URL2, account);
 };
 
 export const forgotPassword = (email) => {
   return axios.post(REST_API_PASSWORD_URL + `/forgot?email=${email}`);
 };
 
-export const verifyPassword = (email,code,newPassword,confirmPassword) => {
-  return axios.post(REST_API_PASSWORD_URL +`/verify?email=${email}&code=${code}&newPassword=${newPassword}&confirmPassword=${confirmPassword}`);
+export const verifyPassword = (email, code, newPassword, confirmPassword) => {
+  return axios.post(REST_API_PASSWORD_URL + `/verify?email=${email}&code=${code}&newPassword=${newPassword}&confirmPassword=${confirmPassword}`);
 };
-
-
