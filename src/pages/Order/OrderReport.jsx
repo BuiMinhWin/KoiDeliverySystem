c3242d6505a7ffe19f5998b8db06b4649c1c4897
Minh Thang <bmthang2004@gmail.com>
2024-11-08 11:12:56 +0700

    update order report

--- src/pages/Order/OrderReport.jsx
+++ src/pages/Order/OrderReport.jsx
@@ -32,7 +32,7 @@ const OrderReport = () => {
     const fetchOrders = async () => {
       try {
         const response = await axios.get(
-          "http://koideliverysystem.id.vn:8080/api/orders"
+          "/api/orders"
         );
         const accountId = localStorage.getItem("accountId");
 
