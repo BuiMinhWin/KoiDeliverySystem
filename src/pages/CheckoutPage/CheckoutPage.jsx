45c8472eee924c5759a62f5371b2c542cbcb50e9
Minh Thang <bmthang2004@gmail.com>
2024-11-10 16:34:01 +0700

    update state for checkout

--- src/pages/CheckoutPage/CheckoutPage.jsx
+++ src/pages/CheckoutPage/CheckoutPage.jsx
@@ -247,7 +247,7 @@ const CheckoutPage = () => {
     if (status === 2) return 2; // "Tài xế nhận đơn"
     if (status === 3) return 3; // "Đã lấy hàng"
     if (status === 4) return 4; //"Đang giao"
-    if (status === 5) return paymentStatus === 2 ? 5 : 6;
+    if (status === 5) return paymentStatus === 0 ? 4 : 5;
     return 0; // Default case
   };
 
@@ -260,7 +260,7 @@ const CheckoutPage = () => {
   // }, {});
 
   return (
-    <Box
+    <Box  
       sx={{
         p: 4,
         bgcolor: "#f5f5f5",
